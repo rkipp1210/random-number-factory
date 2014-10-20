76cd6e345b10fd5ac7bbc7aba6f9a076a8d0fd1d
Ross Kippenbrock <rkippenbrock@gmail.com>
2014-10-20 14:02:15 -0400

    change db to localhost for development

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'url' : 'mongodb://rkippenbrock:password@ds035750.mongolab.com:35750/random-number-factory'
-	//'url' : 'mongodb://localhost/test'
+	//'url' : 'mongodb://rkippenbrock:password@ds035750.mongolab.com:35750/random-number-factory'
+	'url' : 'mongodb://localhost/test'
 }
